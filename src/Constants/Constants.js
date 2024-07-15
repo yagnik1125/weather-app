7bdef8b3b625096aa6778de66f5357c944f54537
yagnik1125 <yagnikvasoya1125@gmail.com>
2024-07-15 12:48:37 +0530

    updated code

--- src/Constants/Constants.js
+++ src/Constants/Constants.js
@@ -1,1 +1,1 @@
-currentWeatherApi='http://api.weatherapi.com/v1/current.json?key=b743c1e6a18e44e3bc0131521240806&q=Rajkot'
+currentWeatherApi='https://api.weatherapi.com/v1/current.json?key=b743c1e6a18e44e3bc0131521240806&q=Rajkot'
